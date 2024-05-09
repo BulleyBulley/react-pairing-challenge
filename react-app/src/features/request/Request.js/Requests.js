8d9dbded1d62e5ef5e3d3f8b12e3fd3a7ab30a25
philbulleyment <philbulleyment@yahoo.co.uk>
2024-05-09 16:15:03 +0100

    initial commit

---
+++ react-app/src/features/request/Request.js/Requests.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Requests() {
+    const [formData, setFormDate] = useState({})
+
+    return (
+      <div>
+      
+       
+      </div>
+    );
+  }
+  
+  export default Requests;
---
+++ react-app/src/utils/object.js
@@ -0,0 +1,9 @@
+export class Feature {
+    constructor(name, description, userId) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.userId = userId;
+        this.votes = [];
+    }
+}
